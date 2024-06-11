a8fd215fc7bcfa4b18e6459bad9ab9d0d57c2ff4
HIPgirl95 <hannah@dawne.com>
2024-06-11 16:38:41 -0400

    Adding Evolutions to Bulbasaur

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 let pokemonList = [
     {name: 'Bulbasaur', height: 70, canEvolve: true, types: ['grass', 'poison']},
+    {name: 'Ivysaur', height: 100, canEvolve: true, types: ['grass', 'poison']},
+    {name: 'Venusaur', height: 200, canEvolve: false, types: ['grass', 'poison']},
     {name: 'Farfetch\'d', height: 80, canEvolve: false, types: ['flying', 'normal']},
     {name: 'Lapras', height: 250, canEvolve: false, types: ['ice', 'water']}
 ]
