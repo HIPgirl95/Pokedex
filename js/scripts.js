956abcf7f2e7eb21600b7ff77d3329161c36180f
HIPgirl95 <hannah@dawne.com>
2024-06-11 16:37:18 -0400

    Adding pokemon to pokemonList

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name: 'Bulbasaur', height: 70, canEvolve: true, types: ['grass', 'poison']},
+    {name: 'Farfetch\'d', height: 80, canEvolve: false, types: ['flying', 'normal']},
+    {name: 'Lapras', height: 250, canEvolve: false, types: ['ice', 'water']}
+]
