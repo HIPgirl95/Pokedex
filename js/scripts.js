ed4f204f21bd0b22b0a7043ebec0ef945d1e20f4
HIPgirl95 <hannah@dawne.com>
2024-06-18 17:25:54 -0400

    Changing to IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 70, canEvolve: true, types: ['grass', 'poison']},
-    {name: 'Ivysaur', height: 100, canEvolve: true, types: ['grass', 'poison']},
-    {name: 'Venusaur', height: 200, canEvolve: false, types: ['grass', 'poison']},
-    {name: 'Farfetch\'d', height: 80, canEvolve: false, types: ['flying', 'normal']},
-    {name: 'Lapras', height: 250, canEvolve: false, types: ['ice', 'water']}
-]
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        {name: 'Bulbasaur', height: 70, canEvolve: true, types: ['grass', 'poison']},
+        {name: 'Ivysaur', height: 100, canEvolve: true, types: ['grass', 'poison']},
+        {name: 'Venusaur', height: 200, canEvolve: false, types: ['grass', 'poison']},
+        {name: 'Farfetch\'d', height: 80, canEvolve: false, types: ['flying', 'normal']},
+        {name: 'Lapras', height: 250, canEvolve: false, types: ['ice', 'water']}
+    ]
+
+    function add(pokemon) {
+        pokemonList.push(pokemon)
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll,
+    };
+})();
 
 // A loop that writes the pokemon's name and height
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write("<p>" + pokemon.name + " (Height: " + pokemon.height + "cm)<br>");
     // Custom comments for varying pokemon heights
     if (pokemon.height >= 250) {
