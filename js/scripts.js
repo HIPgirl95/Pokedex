2e29610c938d08b1916da3b2a89e20b33befa0cd
HIPgirl95 <hannah@dawne.com>
2024-06-25 15:41:42 -0400

    Move Function to IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ let pokemonRepository = (function () {
         {name: 'Lapras', height: 250, canEvolve: false, types: ['ice', 'water']}
     ]
 
+
     function add(pokemon) {
         pokemons.push(pokemon)
     }
@@ -15,20 +16,25 @@ let pokemonRepository = (function () {
     function getAll() {
         return pokemons;
     }
+    
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('pokemon-info');
+        listItem.appendChild(button);
+        pokemonList.appendChild(listItem);
+    }
 
     return {
         add: add,
         getAll: getAll,
+        addListItem: addListItem,
     };
 })();
 
 // A loop that writes the pokemon's name and height
 pokemonRepository.getAll().forEach(function(pokemon) {
-    let pokemonList = document.querySelector('.pokemon-list');
-    let listItem = document.createElement('li');
-    let button = document.createElement('button');
-    button.innerText = pokemon.name;
-    button.classList.add('get-pokemon-info');
-    listItem.appendChild(button);
-    pokemonList.appendChild(listItem);
+    pokemonRepository.addListItem(pokemon)
 });
